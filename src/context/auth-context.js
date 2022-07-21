97014e5b3b49670a3a5a72be740a37e77b6efee4
newravecompress <newravecompress@gmail.com>
2022-07-21 15:07:17 +0300

    Add context

---
+++ src/context/auth-context.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+
+const AuthContext = React.createContext({
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
+})
+
+export const AuthContextProvider = props => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  useEffect(() => {
+    const isLogged = localStorage.getItem('isLoggedIn')
+
+    if (isLogged === '1') {
+      setIsLoggedIn(true)
+    }
+  }, [])
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn')
+    setIsLoggedIn(false)
+  }
+
+  const loginHandler = () => {
+    localStorage.setItem('isLoggedIn', '1')
+    setIsLoggedIn(true)
+  }
+
+  return <AuthContext.Provider value={{
+    isLoggedIn,
+    onLogout: logoutHandler,
+    onLogin: loginHandler
+  }}>{props.children}</AuthContext.Provider>
+}
+
+export default AuthContext
