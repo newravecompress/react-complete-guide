3f7a30d7a21fefe7a3aa7528af11cdb8dbccba6d
newravecompress <newravecompress@gmail.com>
2022-08-12 23:55:46 +0300

    Add movies fetching

--- src/App.js
+++ src/App.js
@@ -1,34 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react'
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from './components/MoviesList'
+import './App.css'
 
-function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+function App () {
+  const [movies, setMovies] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  async function fetchMoviesHandler () {
+    setIsLoading(true)
+    setError(null)
+
+    try {
+      const response = await fetch('https://swapi.dev/api/films/')
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!')
+      }
+
+      const data = await response.json()
+
+      const mappedData = data.results.map(el => {
+        return {
+          id: el.episode_id,
+          title: el.title,
+          openingText: el.opening_crawl,
+          releaseDate: el.release_date,
+        }
+      })
+
+      setMovies(mappedData)
+    } catch (e) {
+      setError(e.message)
+    }
+
+    setIsLoading(false)
+  }
+
+  let content = <p>Found no movies!</p>
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />
+  }
+  if (error) {
+    content = <p>{error}</p>
+  }
+  if (isLoading) {
+    content = <p>Loading...</p>
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        {content}
       </section>
     </React.Fragment>
-  );
+  )
 }
 
-export default App;
+export default App
