705785dd7b68041c1b2f2586408ba2355f700813
newravecompress <newravecompress@gmail.com>
2022-09-06 09:44:43 +0300

    Add simple name input

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,15 +1,47 @@
+import { useRef, useState } from 'react'
+
 const SimpleInput = (props) => {
+  const [name, setName] = useState('')
+  const [nameIsValid, setNameIsValid] = useState(true)
+  const nameInput = useRef()
+
+  const nameHandler = event => {
+    setName(event.target.value)
+  }
+
+  const submitHandler = event => {
+    event.preventDefault()
+
+    if (name.trim() === '') {
+      setNameIsValid(false)
+      return
+    }
+
+    console.log(name)
+    setNameIsValid(true)
+    setName('')
+  }
+
+  const inputClasses = nameIsValid ? 'form-control' : 'form-control invalid'
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={submitHandler}>
+      <div className={inputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInput}
+          type="text"
+          id="name"
+          onChange={nameHandler}
+          value={name}
+        />
+        {!nameIsValid && <p className="error-text">Name must not be empty</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
       </div>
     </form>
-  );
-};
+  )
+}
 
-export default SimpleInput;
+export default SimpleInput
