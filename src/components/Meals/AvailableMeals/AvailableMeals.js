44a5c337b37a80135a41a4252f7f13890f71f20f
newravecompress <newravecompress@gmail.com>
2022-09-29 15:20:47 +0300

    Add fetching meals

--- src/components/Meals/AvailableMeals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals/AvailableMeals.js
@@ -1,39 +1,44 @@
 import cls from './AvailableMeals.module.css'
 import Card from '../../UI/Card/Card'
 import MealItem from '../MealItem/MealItem'
-
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-]
+import { useEffect, useState } from 'react'
+import classes from './AvailableMeals.module.css'
 
 const AvailableMeals = () => {
+  const [meals, setMeals] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch('https://react-http-89110-default-rtdb.europe-west1.firebasedatabase.app/meals.json',)
+      const data = await response.json()
+
+      const loadedMeals = []
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price
+        })
+      }
+
+      setMeals(loadedMeals)
+      setIsLoading(false)
+    }
+    fetchMeals()
+  }, [])
+
+  if (isLoading) {
+    return <section className={classes.MealsLoading}>
+      <p>Loading...</p>
+    </section>
+  }
+
   return <section className={cls.meals}>
     <Card>
       <ul>
-        {DUMMY_MEALS.map(el =>
+        {meals.map(el =>
           <MealItem
             key={el.id}
             name={el.name}
