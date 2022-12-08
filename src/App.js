cee144bdbdaedd4ecfba7b0fa8be5a923b78b736
newravecompress <newravecompress@gmail.com>
2022-12-08 19:16:08 +0300

    Add login form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom'
 
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
+import Layout from './components/Layout/Layout'
+import UserProfile from './components/Profile/UserProfile'
+import AuthPage from './pages/AuthPage'
+import HomePage from './pages/HomePage'
 
-function App() {
+function App () {
   return (
     <Layout>
       <Switch>
-        <Route path='/' exact>
+        <Route path="/" exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
+        <Route path="/auth">
           <AuthPage />
         </Route>
-        <Route path='/profile'>
+        <Route path="/profile">
           <UserProfile />
         </Route>
       </Switch>
     </Layout>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,30 +1,85 @@
-import { useState } from 'react';
+import { useContext, useRef, useState } from 'react'
 
-import classes from './AuthForm.module.css';
+import classes from './AuthForm.module.css'
+import { AuthContext } from '../../store/auth-context'
 
 const AuthForm = () => {
-  const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(true)
+  const [isLoading, setIsLoading] = useState(false)
+  const emailInputRef = useRef()
+  const passwordInputRef = useRef()
+  const authCtx = useContext(AuthContext)
 
   const switchAuthModeHandler = () => {
-    setIsLogin((prevState) => !prevState);
-  };
+    setIsLogin((prevState) => !prevState)
+  }
+
+  const submitHandler = event => {
+    event.preventDefault()
+
+    const enteredEmail = emailInputRef.current.value
+    const enteredPassword = passwordInputRef.current.value
+
+    // Add validation
+    setIsLoading(true)
+    let url
+
+    if (isLogin) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCMazMQJKraXOncFFSrEtvtVpRZ1RbVXtM'
+    } else {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCMazMQJKraXOncFFSrEtvtVpRZ1RbVXtM'
+    }
+
+    fetch(
+      url,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true,
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+      .then(res => {
+        setIsLoading(false)
+        if (res.ok) {
+          return res.json()
+        } else {
+          return res.json().then(data => {
+            let errorMessage = 'Authentication failed'
+            throw new Error(errorMessage)
+          })
+        }
+      })
+      .then(data => {
+        authCtx.login(data.idToken)
+      })
+      .catch(err => {
+        alert(err.message)
+      })
+  }
 
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input type="password" id="password" required ref={passwordInputRef} />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {isLoading && <p>Loading...</p>}
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
@@ -33,7 +88,7 @@ const AuthForm = () => {
         </div>
       </form>
     </section>
-  );
-};
+  )
+}
 
-export default AuthForm;
+export default AuthForm
